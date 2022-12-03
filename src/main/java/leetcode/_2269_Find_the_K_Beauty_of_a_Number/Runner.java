af795926beb0a95ce35a5ac5da6ff6639c379b7c
PaulZhuzliakov <paulparatrooper@gmail.com>
2022-12-04 00:30:15 +0700

    _2269_Find_the_K_Beauty_of_a_Number

---
+++ src/main/java/leetcode/_2269_Find_the_K_Beauty_of_a_Number/Runner.java
@@ -0,0 +1,9 @@
+package leetcode._2269_Find_the_K_Beauty_of_a_Number;
+
+public class Runner {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.divisorSubstrings(240, 2);
+        System.out.println(result);
+    }
+}
