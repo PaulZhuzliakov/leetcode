556b45cb790cf933cbbfb990e4420841cc7a6b70
PaulZhuzliakov <paulparatrooper@gmail.com>
2022-12-03 21:35:02 +0700

    0938_Range_Sum_of_BST

---
+++ src/main/java/leetcode/_938_Range_Sum_of_BST/Runner.java
@@ -0,0 +1,18 @@
+package leetcode._938_Range_Sum_of_BST;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        TreeNode ll = new TreeNode(3);
+        TreeNode lr = new TreeNode(7);
+        TreeNode rr = new TreeNode(18);
+
+        TreeNode l = new TreeNode(5, ll, lr);
+        TreeNode r = new TreeNode(15, null, rr);
+
+        TreeNode root = new TreeNode(10, l, r);
+
+        int result = new Solution().rangeSumBST(root, 7, 15);
+        System.out.println(result);
+    }
+}
