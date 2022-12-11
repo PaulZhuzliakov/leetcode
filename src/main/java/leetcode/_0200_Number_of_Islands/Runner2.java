cf95fc22afee93bbe6a604d21df56abbcefa844f
PaulZhuzliakov <paulparatrooper@gmail.com>
2022-12-12 06:36:06 +0700

    _0200_Number_of_Islands

---
+++ src/main/java/leetcode/_0200_Number_of_Islands/Runner2.java
@@ -0,0 +1,15 @@
+package leetcode._0200_Number_of_Islands;
+
+public class Runner2 {
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+        NumberOfIslands numberOfIslands = new NumberOfIslands();
+        int islands = numberOfIslands.numIslands(grid);
+        System.out.println(islands);
+    }
+}
