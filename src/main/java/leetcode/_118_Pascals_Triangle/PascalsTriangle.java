d932d340e16a5bf2018dc4c7cbcda7c255a6eb05
PaulZhuzliakov <paulparatrooper@gmail.com>
2022-12-12 07:24:32 +0700

    _118_Pascals_Triangle

---
+++ src/main/java/leetcode/_118_Pascals_Triangle/PascalsTriangle.java
@@ -0,0 +1,23 @@
+package leetcode._118_Pascals_Triangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            triangle.add(i, list);
+            for (int j = 0; j <= i; j++) {
+               if (j == 0 || j == i) {
+                   triangle.get(i).add(1);
+               } else if (i - 1 > 0){
+                   triangle.get(i).add(triangle.get(i-1).get(j-1) + triangle.get(i-1).get(j));
+               }
+            }
+        }
+
+        return triangle;
+    }
+}
---
+++ src/main/java/leetcode/_118_Pascals_Triangle/Runner.java
@@ -0,0 +1,11 @@
+package leetcode._118_Pascals_Triangle;
+
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        PascalsTriangle pascalsTriangle = new PascalsTriangle();
+        List<List<Integer>> generate = pascalsTriangle.generate(5);
+        System.out.println(generate);
+    }
+}
