d44aee5efbda36a73c12aaff225025b757e3c659
PaulZhuzliakov <paulparatrooper@gmail.com>
2023-06-16 12:04:22 +0300

    ValidAnagram3

--- src/main/java/leetcode/Arrays_And_Hashing/ValidAnagram.java
+++ src/main/java/leetcode/Arrays_And_Hashing/ValidAnagram.java
@@ -1,24 +1,22 @@
 package leetcode.Arrays_And_Hashing;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> map1 = createMap(s);
-        Map<Character, Integer> map2 = createMap(t);
-
-        return map1.equals(map2);
-    }
-
-    Map<Character, Integer> createMap(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char charAt = s.charAt(i);
-            map.computeIfPresent(charAt, (character, integer) -> ++integer);
-            map.putIfAbsent(charAt, 1);
+        if (s.length() != t.length()) {
+            return false;
         }
-        return map;
+        int[] charCount = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            charCount[s.charAt(i) - 'a']++;
+            charCount[t.charAt(i) - 'a']--;
+        }
+
+        for (int charNumber: charCount) {
+            if (charNumber > 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
@@ -28,4 +26,3 @@ public class ValidAnagram {
         System.out.println(solution.isAnagram("rat", "cara"));
     }
 }
-
