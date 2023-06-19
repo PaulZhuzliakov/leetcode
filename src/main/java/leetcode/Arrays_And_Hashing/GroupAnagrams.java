ef8e129e53546e66abd2f9d9c5d0e3722cbf4a20
PaulZhuzliakov <paulparatrooper@gmail.com>
2023-06-19 04:43:37 +0300

    Group Anagrams 2

--- src/main/java/leetcode/Arrays_And_Hashing/GroupAnagrams.java
+++ src/main/java/leetcode/Arrays_And_Hashing/GroupAnagrams.java
@@ -5,47 +5,25 @@ import java.util.*;
 public class GroupAnagrams {
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-
-        for (String str : strs) {
-            String sortedStr = sortStringAlphabetically(str);
-            map.putIfAbsent(sortedStr, new ArrayList<>());
-            for (Map.Entry<String, List<String>> entry : map.entrySet()) {
-                if (isAnagram(str, entry.getKey())) {
-                    map.get(entry.getKey()).add(str);
-                }
-            }
-        }
-
         List<List<String>> result = new ArrayList<>();
-        for (Map.Entry<String, List<String>> entry: map.entrySet()) {
-            result.add(entry.getValue() );
+        if (strs.length == 0) {
+            return result;
         }
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s: strs) {
+            int[] hash = new int[26];
+            for (char c: s.toCharArray()) {
+                hash[c - 'a']++;
+            }
+            String key = Arrays.toString(hash);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        result.addAll(map.values());
         return result;
     }
 
-    boolean isAnagram(String first, String second) {
-        if (first.length() != second.length()) {
-            return false;
-        }
-        int[] charsAsIntInString = new int[26];
-        for (int i = 0; i < first.length(); i++) {
-            charsAsIntInString[first.charAt(i) - 'a']++;
-            charsAsIntInString[second.charAt(i) - 'a']--;
-        }
-        for (int i : charsAsIntInString) {
-            if (i != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    String sortStringAlphabetically(String unsorted) {
-        char[] charArray = unsorted.toCharArray();
-        Arrays.sort(charArray);
-        return new String(charArray);
-    }
 
     public static void main(String[] args) {
         GroupAnagrams solution = new GroupAnagrams();
