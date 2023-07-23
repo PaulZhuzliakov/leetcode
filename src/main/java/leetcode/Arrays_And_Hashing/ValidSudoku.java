b99a9c29a64f1998e8dd992f1d86167922a7e95a
PaulZhuzliakov <paulparatrooper@gmail.com>
2023-07-23 16:14:25 +0300

    refactor

--- src/main/java/leetcode/Arrays_And_Hashing/ValidSudoku.java
+++ src/main/java/leetcode/Arrays_And_Hashing/ValidSudoku.java
@@ -6,61 +6,34 @@ public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
 
         HashSet<Character> nums = new HashSet<>();
-        int dots;
 
         for (char[] line : board) {
-            dots = 0;
             nums.clear();
             for (char c : line) {
-                if (c == '.') {
-                    dots++;
-                } else {
-                    nums.add(c);
-                }
-            }
-            if (nums.size() + dots != 9) {
-                return false;
+                if (!nums.add(c) && c != '.') return false;
             }
         }
 
         for (int i = 0; i < 9; i++) {
-            dots = 0;
             nums.clear();
             for (int j = 0; j < 9; j++) {
-                if (board[j][i] == '.') {
-                    dots++;
-                } else {
-                    nums.add(board[j][i]);
-                }
-            }
-            if (nums.size() + dots != 9) {
-                return false;
+                if (!nums.add(board[j][i]) && board[j][i] != '.') return false;
             }
         }
 
         for (int y = 0; y < 9; y = y + 3) {
             for (int x = 0; x < 9; x = x + 3) {
-                dots = 0;
+
                 nums.clear();
                 for (int y1 = 0; y1 < 3; y1++) {
                     for (int x1 = 0; x1 < 3; x1++) {
                         char c = board[x + x1][y + y1];
-                        System.out.println("x - " + (x + x1)+"; y - " + (y + y1));
-                        System.out.println(x + x1);
-                        System.out.println("char - " + c);
-                        if (c == '.') {
-                            dots++;
-                        } else {
-                            nums.add(c);
-                        }
+                        if (!nums.add(c) && c != '.') return false;
                     }
-                }
-                System.out.println("--");
-                if (nums.size() + dots != 9) {
-                    return false;
                 }
             }
         }
+
         return true;
     }
 
