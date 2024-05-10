9cc4d2e292d32f9e116cffd1a71581cdbee7858e
PaulZhuzliakov <paulparatrooper@gmail.com>
2024-05-10 07:52:58 +0300

    234. Palindrome Linked List

---
+++ src/main/java/leetcode/_0234_Palindrome_Linked_List/Main.java
@@ -0,0 +1,55 @@
+package leetcode._0234_Palindrome_Linked_List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().isPalindrome(
+                new ListNode(1,
+                        new ListNode(2,
+                                new ListNode(2,
+                                        new ListNode(1))))));   //Output: true
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow = reversed(slow);
+        fast = head;
+        while (slow != null) {
+            if (slow.val != fast.val) return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    private ListNode reversed(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
